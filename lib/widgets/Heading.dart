1f165403777008c3c5d139863ac936b64f7f8335
Rosyid Rosadi <rosyidrosadi15@gmail.com>
2025-02-12 20:54:50 +0700

    feat(state): implement Stateless widget

---
+++ lib/widgets/Heading.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Heading extends StatelessWidget {
+
+  final String text;
+
+  const Heading({Key? key, required this.text}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
